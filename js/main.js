7673d3a7b2a76bef5cc59c09e9e697fa989cc940
Brandy Bachman <bachman.brandyl@gmail.com>
2026-06-21 20:59:15 -0400

    Add loading screen for StoryMap iframe

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 // Set copyright year
 document.getElementById("copyright-year").textContent = new Date().getFullYear();
+
+// When StoryMap is loading, show the loading wheel and message
+function storyLoaded() {
+    // Give the StoryMap a couple extra seconds to finish rendering
+    setTimeout(() => {
+        document.getElementById("loading").style.display = "none";
+    }, 2000);
+}
